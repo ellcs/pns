1d6c06f9921454340e254dc5d4acdb79e7ad694a
Alex Mantel (ellcs) <alexmantel93@gmail.com>
2017-06-15 23:57:39 +0200

    Add interfaces to fw-rules and exercise 4

--- scripts/firewall_rules/fwintern.sh
+++ scripts/firewall_rules/fwintern.sh
@@ -20,17 +20,17 @@ iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
 
 # intern
 # jedes ausgehende packet ist okay.
-iptables -A FORWARD -s $client -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $eth_client -s $client -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 # jedes antwort-packet soll zu intern gehen koennen.
-iptables -A FORWARD -d $client -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -o $eth_client -d $client -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # backend
 # jedes ausgehende packet ist okay.
-iptables -A FORWARD -s $backend -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -i $eth_backend -s $backend -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 # jedes antwort-packet soll zu backend gehen koennen.
-iptables -A FORWARD -s $server -d $backend -j ACCEPT
-iptables -A FORWARD -s $client -d $backend -j ACCEPT
-iptables -A FORWARD -d $backend -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i $eth_server -o $eth_backend -s $server -d $backend -j ACCEPT
+iptables -A FORWARD -i $eth_client -o $eth_backend -s $client -d $backend -j ACCEPT
+iptables -A FORWARD -o $eth_backend -d $backend -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 iptables --policy INPUT DROP
 iptables --policy FORWARD DROP
--- scripts/firewall_rules/fwworld.sh
+++ scripts/firewall_rules/fwworld.sh
@@ -22,30 +22,30 @@ iptables -A OUTPUT -p tcp --sport 22 -j ACCEPT
 # fextern
 # http server der firma
 # MASQUERADE aktiviert das NATing. Das interne interface muss angegeben werden.
-iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+iptables -t nat -A POSTROUTING -o $eth_server -j MASQUERADE
 
-iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 80 -j DNAT --to "$server1:80"
-iptables -A FORWARD -i eth1 -p tcp --dport 80 -d "$server1" -j ACCEPT
-iptables -A FORWARD -i eth0 -p tcp --sport 80 -s "$server1" -j ACCEPT
+iptables -t nat -A PREROUTING -i $eth_world -p tcp --dport 80 -j DNAT --to "$server1:80"
+iptables -A FORWARD -i $eth_world -p tcp --dport 80 -d "$server1" -j ACCEPT
+iptables -A FORWARD -i $eth_server -p tcp --sport 80 -s "$server1" -j ACCEPT
 
 # imap
-iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 143 -j DNAT --to "$server2:143"
-iptables -A FORWARD -i eth0 -p tcp --dport 143 -d "$server2" -j ACCEPT
-iptables -A FORWARD -i eth1 -p tcp --sport 143 -s "$server2" -j ACCEPT
+iptables -t nat -A PREROUTING -i $eth_world -p tcp --dport 143 -j DNAT --to "$server2:143"
+iptables -A FORWARD -i  $eth_world -p tcp --dport 143 -d "$server2" -j ACCEPT
+iptables -A FORWARD -i  $eth_server -p tcp --sport 143 -s "$server2" -j ACCEPT
 
 # smtp
-iptables -t nat -A PREROUTING -i eth1 -p tcp --dport 25 -j DNAT --to "$server3:25"
-iptables -A FORWARD -i eth0 -p tcp --dport 25 -d "$server3" -j ACCEPT
-iptables -A FORWARD -i eth0 -p tcp --sport 25 -s "$server3" -j ACCEPT
+iptables -t nat -A PREROUTING -i $eth_world -p tcp --dport 25 -j DNAT --to "$server3:25"
+iptables -A FORWARD -i $eth_world -p tcp --dport 25 -d "$server3" -j ACCEPT
+iptables -A FORWARD -i $eth_server -p tcp --sport 25 -s "$server3" -j ACCEPT
 
 # intern - stateful
-iptables -A FORWARD -s $client -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -d $client -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i $eth_server -s $client -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -o $eth_server -d $client -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # backend - stateful
-iptables -A FORWARD -s $backend -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -d $backend -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i $eth_server -s $backend -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -o $eth_server -d $backend -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # server - stateful
-iptables -A FORWARD -s $server -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
-iptables -A FORWARD -d $server -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -i $eth_server -s $server -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A FORWARD -o $eth_server -d $server -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
